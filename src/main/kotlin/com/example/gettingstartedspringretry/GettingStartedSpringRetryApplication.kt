9e1a9d444dd88f43a9e2b617fbfb1acedc7a1a44
junhee-ko <junheee.ko@gmail.com>
2024-05-19 14:40:10 +0900

    first commit

---
+++ src/main/kotlin/com/example/gettingstartedspringretry/GettingStartedSpringRetryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gettingstartedspringretry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GettingStartedSpringRetryApplication
+
+fun main(args: Array<String>) {
+    runApplication<GettingStartedSpringRetryApplication>(*args)
+}
